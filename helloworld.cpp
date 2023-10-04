4d76e15701a89cd3fb52696434a11a86deefac12
subodh_bhalerao <imsubodh007@gmail.com>
2023-10-04 21:03:42 +0530

    hello

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    cout << "\n";
+    cout << "Kasa Kay? bara ahe ka?";
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "setting up with c++";  
+    return 0;
+}
