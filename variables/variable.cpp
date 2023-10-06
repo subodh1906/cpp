903b1b555e0c08fb9e9636e3dcd3f7d276eb3bc9
subodh_bhalerao <imsubodh007@gmail.com>
2023-10-06 12:14:17 +0530

    ur comment

---
+++ variables/variable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // dataType variableName = value 
+    
+    string myName = "subodh";
+    myName = "Bhalerao";
+    cout << myName;
+
+    cout << "\n";      
+    // "\n"  is for new line 
+
+    bool isAdmin = true;
+    cout << isAdmin;
+
+    return 0;
+
+}
+    /* 
+        double line comment shift + alt + A
+        single line comment ctrl + /
+
+        variables are like containers used to store data values.
+     */
